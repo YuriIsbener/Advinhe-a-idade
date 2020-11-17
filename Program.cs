6c5557f7ab4d23c70dd028517260b198a684bb53
Yuri Isbener <yurisuhett@gmail.com>
2020-11-17 16:02:41 -0300

    Atividde Feita

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Advinhe_a_idade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a idade do ator");
+            int idade = int.Parse(Console.ReadLine());
+            
+            while (idade != 47)
+            {
+                Console.WriteLine("Errou!");
+                Console.WriteLine("Digite a idade do ator novamente");
+                idade = int.Parse(Console.ReadLine());
+
+            }
+            Console.WriteLine("Acertou Miseravi");
+        }
+    }
+}
